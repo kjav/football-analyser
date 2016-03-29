a219caf501ec005873ba608d67df2890ec44508c
saitikas <aidkilda@gmail.com>
2016-03-29 22:43:17 +0300

    First draft of a model

---
+++ analysis/player_contribution.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 29 22:18:37 2016
+
+@author: Aidas
+"""
+
+def playerAttackContribution(player):
+    return 0
+    
+def playerDefenseContribution(player):
+    return 0
