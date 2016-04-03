b50c73437f96140f7c9fb78ec6cb1d5a6322de22
saitikas <aidkilda@gmail.com>
2016-04-03 10:25:37 +0300

    Updated model

--- analysis/model.py
+++ analysis/model.py
@@ -22,18 +22,13 @@ import pymc as pm
 homeGoalAvg = 0.395
 awayGoalAvg = 0.098
 
-# Small constatnt to acoid sampler getting stuck
-C = 0.0001
-
-#Scaling-specific parameter for beta distributions
-S = 10
-
-#Loss of memory effect for beta distributions
-eps = 3
-rho = 10
-
 tau = pm.Gamma('tau', 10, 1)
 precision = pm.Gamma('precision', 10, 0.1)
+
+# Data from website
+####################################################
+
+####################################################
 
 # Stupid initialization
 # TODO CHANGE  IT
@@ -41,6 +36,7 @@ precision = pm.Gamma('precision', 10, 0.1)
 noTeams = 20
 noTimeslices = 38
 noGames = 200
+obsRounds = 10
 
 ####################################################
 unscaledattack = [[None]*noTeams for i in range(noTimeslices)]
@@ -48,29 +44,14 @@ unscaleddefense = [[None]*noTeams for i in range(noTimeslices)]
 ascale = [[None]*noTeams for i in range(noTimeslices)]
 dscale = [[None]*noTeams for i in range(noTimeslices)]
 
+
 attack = [[None]*noTeams for i in range(noTimeslices)]
 defense = [[None]*noTeams for i in range(noTimeslices)]
-passSuccess = [[None]*noTeams for i in range(noTimeslices)]
-aerialSuccess = [[None]*noTeams for i in range(noTimeslices)]
-contestSuccess = [[None]*noTeams for i in range(noTimeslices)]
-possession = [[None]*noTeams for i in range(noTimeslices)]
-attempts = [[None]*noTeams for i in range(noTimeslices)]
-saves = [[None]*noTeams for i in range(noTimeslices)]
 motion = [[None]*noTeams for i in range(noTimeslices)]
 days = [[None]*noTeams for i in range(noTimeslices)]
-betaScale = [[None]*noTeams for i in range(noTimeslices)]
 
-playSkillDiff = [None] * noGames
 team = [[None]*noGames for i in range(2)]
 timeslice = [[None]*noGames for i in range(2)]
-hAttSkill = [None] * noGames
-hDefSkill = [None] * noGames
-aAttSkill = [None] * noGames
-aDefSkill = [None] * noGames
-wHomeAtt = [None] * noGames
-wHomeDef = [None] * noGames
-wAwayAtt = [None] * noGames
-wAwayDef = [None] * noGames
 delta = [None] * noGames
 homeLambda = [None] * noGames
 awayLambda = [None] * noGames
@@ -79,27 +60,16 @@ goalsScored = [[None]*noGames for i in range(2)]
 
 ####################################################
 
+### TODO: Change python lists to pymc arrays
 # Loop though all teams and all timeslices
 for t in range(0 ,noTeams):
     # Initial distribution of attack/defence strength
-    unscaledattack[t, 1] = pm.Normal('unscaledattack_%t' % t, 0, precision)
-    unscaleddefense[t, 1] = pm.Normal('unscaleddefence_%t' % t, 0, precision)
+    unscaledattack[t, 1] = pm.Normal('unscaledattack%d' % t, 0, precision)
+    unscaleddefense[t, 1] = pm.Normal('unscaleddefence%d' % t, 0, precision)
 
     attack[t, 1] = unscaledattack[t, 1]
     defense[t, 1] = unscaleddefense[t, 1]
 
-    
-    # Initial distribution of other skill parameters
-    # Add C to avoid sampler getting stuck at inf
-    #TODO look at truncating
-    
-    passSuccess[t,1] = pm.Beta('passSuccess_%t' % t, 0.7*S, 0.3*S)
-    aerialSuccess[t,1] = pm.Beta('aerialSuccess_%t' % t, 0.5*S, 0.5*S)
-    contestSuccess[t,1] = pm.Beta('contestSuccess_%t' % t, 0.5*S, 0.5*S)
-    possession[t,1] = pm.Beta('possession_%t' % t, 0.5*S, 0.5*S)
-    attempts[t,1] = pm.Beta('attempts_%t' % t, 0.5*S, 0.5*S)
-    saves[t,1] = pm.Beta('saves_%t' % t, 0.7*S, 0.3*S)
-    
     # Evolve the attack/defence strength over to,e with Brownian motion (Wiener process).
     
     # The parameters are normally distributed with mean equal to the parameter value of
@@ -113,27 +83,6 @@ for t in range(0 ,noTeams):
         
         unscaleddefense[t,s] = pm.Normal(unscaleddefense[t, s-1], motion[t,s])
         
-        betaScale[t, s] = S - (abs(days[t,s] - days[t, s-1])) / rho
-        
-        passSuccess[t,s] = pm.Beta((passSuccess[t, s-1] + C) * betaScale[t,s],
-                                  (1- passSuccess[t, s-1] + C) * betaScale[t,s])
-                                  
-        aerialSuccess[t,s] = pm.Beta((aerialSuccess[t, s-1] + C) * betaScale[t,s],
-                                  (1- aerialSuccess[t, s-1] + C) * betaScale[t,s]) 
-                                  
-        contestSuccess[t,s] = pm.Beta((contestSuccess[t, s-1] + C) * betaScale[t,s],
-                                  (1- contestSuccess[t, s-1] + C) * betaScale[t,s])
-                                  
-        possession[t,s] = pm.Beta((possession[t, s-1] + C) * betaScale[t,s],
-                                  (1- possession[t, s-1] + C) * betaScale[t,s])
-                                  
-        attempts[t,s] = pm.Beta((attempts[t, s-1] + C) * betaScale[t,s],
-                                  (1- attempts[t, s-1] + C) * betaScale[t,s])
-        
-        saves[t,s] = pm.Beta((saves[t, s-1] + C) * betaScale[t,s],
-                                  (1- saves[t, s-1] + C) * betaScale[t,s])
-    
-
         attack[t, s] = unscaledattack[t,s] + math.log(ascale[s, t])
         
         defense[t, s] = unscaleddefense[t,s] + math.log(dscale[s, t])
@@ -153,73 +102,30 @@ for t in range(0 ,noTeams):
 
 gamma = pm.Uniform('gamma', 0, 0.1)
 
-# Scalers for limiting impact of att/def skill
-attSkillScaler = pm.Uniform('attSkillScaler', 0, 0.25)
-defSkillScaler = pm.Uniform('defSkillScaler', 0, 0.25)
-
 # Loop through all games in correct order
 for i in range(0, noGames):
-    # Home team open play skill
-    playSkillDiff[i] = (passSuccess[team[i, 1], timeslice[i,1]] -
-                        passSuccess[team[i, 2], timeslice[i,2]] +
-                        aerialSuccess[team[i, 1], timeslice[i,1]] -
-                        aerialSuccess[team[i, 2], timeslice[i,2]] +
-                        contestSuccess[team[i, 1], timeslice[i,1]] -
-                        contestSuccess[team[i, 2], timeslice[i,2]] +
-                        possession[team[i, 1], timeslice[i,1]] -
-                        possession[team[i, 2], timeslice[i,2]]
-                    )
-                    
-    # Home team attack - specific skill
-    hAttSkill[i] = (playSkillDiff[i] +
-                    attempts[team[i, 1], timeslice[i, 1]] -
-                    saves[team[i, 2], timeslice[i, 2]]) / 5
-    
-    # Home team defense - specific skill
-    hDefSkill[i] = (playSkillDiff[i] +
-                    saves[team[i, 1], timeslice[i, 1]] -
-                    attempts[team[i, 2], timeslice[i, 2]]) / 5
-    
-    # Away team attack specific skill
-    aAttSkill[i] = 0 - hDefSkill[i]
-    
-    # Away team defense specific skill
-    aDefSkill[i] = 0 - hAttSkill[i]
-    
-    # Attack/defense node mutators
-    wHomeAtt[i] = (hAttSkill[i] * attSkillScaler) + attack[team[i, 1], timeslice[i, 1]]
-    wHomeDef[i] = (hDefSkill[i] * defSkillScaler) + defense[team[i, 1], timeslice[i, 1]]
-    wAwayAtt[i] = (aAttSkill[i] * attSkillScaler) + attack[team[i, 2], timeslice[i, 2]]
-    wAwayDef[i] = (aAttSkill[i] * defSkillScaler) + attack[team[i, 2], timeslice[i, 2]]
 
     # delta param for psychological effect of underestimating
-    delta[i] = (wHomeAtt[i] + wHomeDef[i] -
-                wAwayAtt[i] - wAwayDef[i]) /2
-                
+    delta[i] = (attack[team[i, 1], timeslice[i, 1]] + 
+                defense[team[i, 1], timeslice[i, 1]] -
+                attack[team[i, 2], timeslice[i, 2]] -
+                defense[team[i, 2], timeslice[i, 2]]) / 2
+
     # Home lambda parameter
-    homeLambda[i] = pm.Exponential(homeGoalAvg + wHomeAtt[i] - wAwayDef[i] - gamma * delta[i])
+    @pm.deterministic
+    def homeLambda(i):
+        return math.exp(homeGoalAvg + attack[team[i, 1], timeslice[i, 1]] - 
+                            defense[team[i, 2], timeslice[i, 2]] - gamma * delta[i])
     
     # Away lambda parameter
-    awayLambda[i] = pm.Exponential(awayGoalAvg + wAwayAtt[i] - wHomeDef[i] + gamma * delta[i])
+    @pm.deterministic
+    def awayLambda(i):
+        return math.exp(awayGoalAvg + attack[team[i, 2], timeslice[i, 2]] - 
+                defense[team[i, 1], timeslice[i, 1]] + gamma * delta[i])    
     
     
     goalsScored[i, 1] = pm.Poisson(homeLambda[i])
     goalsScored[i, 2] = pm.Poisson(awayLambda[i])
-    
-
-    
-
-    
-
-    
-    
-    
-    
-    
-    
-    
-    
-    
     
     
     
