49641f1e112a7d81d5d4b427bf8c293cb4823530
pasta3049 <morgan.king@keble.ox.ac.uk>
2016-04-17 08:38:41 +0100

    Update 02_init.sql
    Added probableLineUp table

--- football_db/migrations/02_init.sql
+++ football_db/migrations/02_init.sql
@@ -9,6 +9,24 @@ CREATE TABLE match (
     the_date date NOT NULL
 );
 
+CREATE TABLE probableLineUp (
+    id bigserial PRIMARY KEY,
+    matchid bigserial REFERENCES match(id) NOT NULL,
+    teamid bigserial REFERENCES team(id) NOT NULL,
+    player1id REFERENCES player(id) NOT NULL,
+    player2id REFERENCES player(id) NOT NULL,
+    player3id REFERENCES player(id) NOT NULL,
+    player4id REFERENCES player(id) NOT NULL,
+    player5id REFERENCES player(id) NOT NULL,
+    player6id REFERENCES player(id) NOT NULL,
+    player7id REFERENCES player(id) NOT NULL,
+    player8id REFERENCES player(id) NOT NULL,
+    player9id REFERENCES player(id) NOT NULL,
+    player10id REFERENCES player(id) NOT NULL,
+    player11id REFERENCES player(id) NOT NULL
+);
+
 -- rambler down
 
 DROP TABLE match;
+DROP TABLE probableLineUp;
