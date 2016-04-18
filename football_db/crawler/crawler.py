82cba7aa78e212a92a8314142fedc1fb9d3f2e98
nbcks <nicholas.buckner@balliol.ox.ac.uk>
2016-04-18 01:28:05 +0100

    python crawler now can generate the source of a whoscored website and extract the json into one pretty printed json file

--- football_db/crawler/crawler.py
+++ football_db/crawler/crawler.py
@@ -1,24 +1,53 @@
-import sys  
-from PyQt4.QtGui import *  
-from PyQt4.QtCore import *  
-from PyQt4.QtWebKit import *  
-from lxml import html 
+import re
+import json
+#from selenium import webdriver
 
-class Render(QWebPage):  
-  def __init__(self, url):  
-    self.app = QApplication(sys.argv)  
-    QWebPage.__init__(self)  
-    self.loadFinished.connect(self._loadFinished)  
-    self.mainFrame().load(QUrl(url))  
-    self.app.exec_()  
-  
-  def _loadFinished(self, result):  
-    self.frame = self.mainFrame()  
-    self.app.quit() 
 
-url = 'http://www.whoscored.com/Matches/614052/Live'  
-#This does the magic.Loads everything
-r = Render(url)  
-#result is a QString.
-result = r.frame.toHtml()
-print result.toUtf8()
+# These are all the regex's to get the JSON from the page source
+allRegionsReg = re.compile("var allRegions = (?P<data>\[.*?\]);", re.DOTALL)
+matchCentreDataReg = re.compile("var matchCentreData = (?P<data>.*?);", re.DOTALL)
+matchCentreEventTypeJsonReg = re.compile("var matchCentreEventTypeJson = (?P<data>.*?);", re.DOTALL)
+matchIdReg = re.compile("var matchId = (?P<data>.*?);", re.DOTALL)
+formationIdNameMappingsReg = re.compile("var formationIdNameMappings = (?P<data>.*?);", re.DOTALL)
+
+def get_JSON(page_source):
+    unprocessedregions = allRegionsReg.search(page_source)
+    allRegions = unprocessedregions.group("data") if unprocessedregions else '{}'
+
+    unprocessedMatchCentreData = matchCentreDataReg.search(page_source)
+    MatchCentreData =  unprocessedMatchCentreData.group("data") if unprocessedMatchCentreData else '{}'
+
+    unprocEventType = matchCentreEventTypeJsonReg.search(page_source)
+    EventType  = unprocEventType.group("data") if unprocEventType else '{}'
+
+    unprocid = matchIdReg.search(page_source, re.DOTALL)
+    matchId = unprocid.group("data") if unprocid else '{}'
+
+    unprocform = formationIdNameMappingsReg.search(page_source, re.DOTALL)
+    formationIdMapping = unprocform.group("data") if unprocform else '{}'
+
+    unpretty = '{"matchId":' + matchId + ',"allRegions":' + '"'+ "" + '"' + ',"matchCentreData":' + MatchCentreData + ',"matchCentreEventTypeJson":' + EventType + ',"formationIdNameMappings":' + formationIdMapping + '}'
+    decoded = json.loads(unpretty)
+    pretty = json.dumps(decoded, indent=4, sort_keys=True)
+    return pretty
+
+start = 0
+end = 10
+everton = 614052
+
+everton_match_url = "http://www.whoscored.com/Matches/614052/Live"
+python_url = "http://www.python.org"
+
+"""
+driver = webdriver.Firefox()
+driver.get(everton_match_url)
+html_source = driver.page_source
+print get_JSON(html_source.encode("utf-8"))
+driver.close()
+"""
+
+test = open("everton_source.html")
+html_source = test.read()
+test.close()
+print get_JSON(html_source)
+
