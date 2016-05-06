dd20f92b3684d40ea8b7cdfab1b15dc974d75f15
Sam <sam.rr.lee@gmail.com>
2016-05-06 10:13:08 +0100

    added editorial page

--- server.js
+++ server.js
@@ -6,10 +6,44 @@ var path = __dirname + '/frontend/views/';
 var pg = require('pg');
 var qs = require('querystring');
 var url = require('url');
+var spawn = require('child_process').spawn;
 
 var swig = new swig.Swig();
 app.engine('html', swig.renderFile);
 app.set('view engine', 'html');
+
+var simulator = spawn('python', ['analysis/simulation.py']);
+var received = '';
+var simulator_loaded = false;
+var awaiting_results = null;
+var stdout_buff = simulator.stdout.on('data', function(data) {
+  if (!simulator_loaded) {
+    received = received + data;
+    if (~received.indexOf('ready for input\n')) {
+      console.log('Simulator loaded!');
+      simulator_loaded = true;
+    }
+  } else {
+    var result = '' + data;
+    if (awaiting_results) {
+      awaiting_results(result);
+    }
+  }
+});
+var stderr_buff = simulator.stderr.on('data', function(data) {
+  console.log('Simulation error: ' + data);
+});
+
+function simulate(team1, team2, lineup1, lineup2, callback) {
+  awaiting_results = function(data) {
+    callback(data);
+    awaiting_results = null;
+  };
+  simulator.stdin.write(team1 + '\n');
+  simulator.stdin.write(team2 + '\n');
+  simulator.stdin.write(lineup1 + '\n');
+  simulator.stdin.write(lineup2 + '\n');
+}
 
 var conString = "postgres://coxswain:Torpids2016@football.c7v4rmvelqsx.eu-west-1.rds.amazonaws.com/postgres";
 //this initializes a connection pool
@@ -19,6 +53,10 @@ var conString = "postgres://coxswain:Torpids2016@football.c7v4rmvelqsx.eu-west-1
 router.use(function (req,res,next) {
   console.log("/" + req.method);
   next();
+});
+
+router.get("/editorial", function(req, res) {
+  res.render(path + "editorial.html");
 });
 
 router.get("/",function(req,res){
@@ -41,6 +79,7 @@ router.get('/lineup', function(req, res) {
 
 router.get('/players', function(req, res) {
   pg.connect(conString, function(err, client, done) {
+    if (err) console.log(err);
     client.query('SELECT name, id FROM player', [], function(err, result) {
       if (err) console.log(err);
       done();
@@ -72,10 +111,12 @@ router.get('/teammatches', function(req, res) {
 router.get("/player",function(req,res){
   var q = qs.parse(url.parse(req.url).query)
   pg.connect(conString, function(err, client, done) {
+    if (err) console.log(err);
     client.query('SELECT name, Position, Height, Division, apps, Goals, age, LValue, PredictedValue  FROM player WHERE id='+q.id, [], function(err, result) {
       if (err) console.log(err);
       done();
       var player = result.rows[0];
+      console.log(player);
       client.query('SELECT team.name AS teamname, team.id as teamid, other_team.name AS otherteamname, other_team.id as otherteamid, player_match.Position, player_match.Goals, player_match.TotalPass FROM player_match INNER JOIN team_match ON team_match.id=teammatchid INNER JOIN team ON team_match.team=team.id INNER JOIN match ON match.id=team_match.match INNER JOIN team_match other_team_match ON other_team_match.match=match.id AND other_team_match.team!=team.id INNER JOIN team other_team ON other_team.id=other_team_match.team WHERE playerid=' + q.id, [], function(err, result) {
         if (err) console.log(err);
         var playermatches = result.rows;
@@ -103,6 +144,11 @@ router.get("/team",function(req,res){
   });
 });
 
+router.get("/an", function(req, res) {
+  simulate('1', '2', '1 2 3 4 5 6 7 8 9 10 11', '12 13 14 15 16 17 18 19 20 21 22', function(data) {
+    res.end(data);
+  });
+});
 app.use("/",router);
 
 app.use(express.static('./frontend/public'));
